b520eb761fd2e69d9d9ab573d174a32f103755bc
Unknown <kbatman37@gmail.com>
2016-12-17 14:20:34 -0600

    Basic interupt / sleep version working

--- HelloWorld/HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld/HelloWorld.cpp
@@ -1,14 +1,7 @@
-/*
- * HelloWorld.cpp
- *
- * Created: 12/12/2016 4:09:43 PM
- *  Author: daviklu
- */ 
-
-// Basic version, turn an LED on
-
 #include <avr/io.h>
 #include <util/delay.h>
+#include <avr/interrupt.h>
+#include <avr/sleep.h>
 
 #define F_CPU 1000000UL // we are specifying 1MHz clock frequency (as a unsigned long)
 
@@ -17,24 +10,45 @@
 #define output_high(port,pin) port |= (1<<pin)
 #define set_input(portdir,pin) portdir &= ~(1<<pin)
 #define set_output(portdir,pin) portdir |= (1<<pin)
+#define enableInterupt(port,pin) port |= (1 << pin);
+#define isInputHigh(port,pin) !(port & (1<<pin))
+#define enableInteruptsGlobally() GIMSK |= (1 << PCIE);
+
+
+
+// the handler for the interrupt of pin1
+ISR(PCINT_vect){
+	if(isInputHigh(PINB,PB1))
+	{
+		output_high(PORTB,PB4);
+	}
+	else
+	{
+		output_low(PORTB,PB4);
+	}
+}
 
 int main(void)
 {
 	// set pin to output
 	set_output(DDRB,PB4);
 	
-    while(1)
-    {
-		// set pin high
-		output_high(PORTB,PB4);
+	// set port B1 to input
+	set_input(DDRB, PB1);
+	
+	// Set the sleep mode so that when it sleeps it powers down
+	set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+	
+	// set interrupt for B1
+	enableInterupt(PCMSK,PB1);
+	enableInteruptsGlobally();
 
-		// delay
-		_delay_ms(1000);
-
-		// set pin low
-		output_low(PORTB,PB4);
-
-		// delay
-		_delay_ms(1000);
-    }
+	// turn on interrupts globally
+	sei();
+	
+	while(1)
+	{
+		// Tell it to sleep
+		sleep_mode();
+	}
 }
