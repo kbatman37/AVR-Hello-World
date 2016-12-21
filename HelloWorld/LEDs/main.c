2b223634b2c932e07981f00f68b7e8522bf62913
Unknown <kbatman37@gmail.com>
2016-12-20 20:58:48 -0600

    Working demo

--- HelloWorld/LEDs/main.c
+++ HelloWorld/LEDs/main.c
@@ -26,26 +26,76 @@
 
 #define F_CPU 1000000UL // we are specifying 1MHz clock frequency (as a unsigned long)
 
+#define ENABLE_INTERUPTS_GLOBALLY GIMSK |= (1 << PCIE);
+#define TRUE 1
+#define FALSE 0
+typedef unsigned char bool;
+
+//volatile bool interuptSeen;
+
 void Initialize()
 {
 	LED1_SET_OUTPUT;
 	LED2_SET_OUTPUT;
-	LED3_SET_OUTPUT;
-	LED4_SET_OUTPUT;
 
 	BUTTON1_SET_INPUT;
+	BUTTON1_SET_HIGH; // set pull-up resistor
+
 	BUTTON2_SET_INPUT;
-	BUTTON3_SET_INPUT;
-	BUTTON4_SET_INPUT;
+	BUTTON2_SET_HIGH; // set pull-up resistor
+
+	// set interrupt for B1
+	BUTTON1_ENABLE_INTERUPT;
+	BUTTON2_ENABLE_INTERUPT;
+	ENABLE_INTERUPTS_GLOBALLY;
+	sei();
 }
+
+
+
+ISR(PCINT_vect){
+	cli();
+	
+	_delay_ms(500);
+
+	if(BUTTON1_IS_LOW)
+	{
+		if(BUTTON2_IS_LOW){
+			LED1_SET_HIGH;
+			LED2_SET_HIGH;
+		}
+		else{
+			LED1_SET_HIGH;
+			LED2_SET_LOW;
+		}
+	}
+	else
+	{
+		if(BUTTON2_IS_LOW){
+			LED1_SET_LOW;
+			LED2_SET_HIGH;
+		}
+		else{
+			LED1_SET_LOW;
+			LED2_SET_LOW;
+		}
+	}
+
+	_delay_ms(500);
+	LED1_SET_LOW;
+	LED2_SET_LOW;
+	
+	sei();
+}
+
 
 int main(void)
 {
 	Initialize();
-
     /* Replace with your application code */
     while (1) 
     {
+		sleep_mode();		
     }
 }
 
